bb1ff4cb42ec88b6e35da150254398809b4c90b3
NavidNasyry <navidnasyry@gmail.com>
2021-06-15 13:09:30 +0430

    save file - open file

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,12 +29,208 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.txt_code = new System.Windows.Forms.TextBox();
+            this.lbl_code = new System.Windows.Forms.Label();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripSplitButton1 = new System.Windows.Forms.ToolStripSplitButton();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveFileDialog2 = new System.Windows.Forms.SaveFileDialog();
+            this.lbl_file_name = new System.Windows.Forms.Label();
+            this.lbl_current_file_name = new System.Windows.Forms.Label();
+            this.loadFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.datagrid_ram = new System.Windows.Forms.DataGridView();
+            this.Lable = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Address = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Instruction = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Hex = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.form1BindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.toolStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.datagrid_ram)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.form1BindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txt_code
+            // 
+            this.txt_code.Font = new System.Drawing.Font("SimSun-ExtB", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_code.Location = new System.Drawing.Point(27, 81);
+            this.txt_code.Multiline = true;
+            this.txt_code.Name = "txt_code";
+            this.txt_code.Size = new System.Drawing.Size(329, 622);
+            this.txt_code.TabIndex = 0;
+            this.txt_code.TextChanged += new System.EventHandler(this.Txt_code_TextChanged);
+            // 
+            // lbl_code
+            // 
+            this.lbl_code.AutoSize = true;
+            this.lbl_code.Location = new System.Drawing.Point(24, 61);
+            this.lbl_code.Name = "lbl_code";
+            this.lbl_code.Size = new System.Drawing.Size(79, 17);
+            this.lbl_code.TabIndex = 1;
+            this.lbl_code.Text = "Enter Code";
+            this.lbl_code.Click += new System.EventHandler(this.Label1_Click);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripSplitButton1});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(1178, 27);
+            this.toolStrip1.TabIndex = 2;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripSplitButton1
+            // 
+            this.toolStripSplitButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripSplitButton1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.saveToolStripMenuItem,
+            this.openFileToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.toolStripSplitButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripSplitButton1.Image")));
+            this.toolStripSplitButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripSplitButton1.Name = "toolStripSplitButton1";
+            this.toolStripSplitButton1.Size = new System.Drawing.Size(39, 24);
+            this.toolStripSplitButton1.Text = "toolStripSplitButton1";
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(147, 26);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.SaveToolStripMenuItem_Click);
+            // 
+            // openFileToolStripMenuItem
+            // 
+            this.openFileToolStripMenuItem.Name = "openFileToolStripMenuItem";
+            this.openFileToolStripMenuItem.Size = new System.Drawing.Size(147, 26);
+            this.openFileToolStripMenuItem.Text = "Open File";
+            this.openFileToolStripMenuItem.Click += new System.EventHandler(this.OpenFileToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(147, 26);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.ExitToolStripMenuItem_Click);
+            // 
+            // lbl_file_name
+            // 
+            this.lbl_file_name.AutoSize = true;
+            this.lbl_file_name.Location = new System.Drawing.Point(24, 44);
+            this.lbl_file_name.Name = "lbl_file_name";
+            this.lbl_file_name.Size = new System.Drawing.Size(83, 17);
+            this.lbl_file_name.TabIndex = 3;
+            this.lbl_file_name.Text = "File Name : ";
+            this.lbl_file_name.Click += new System.EventHandler(this.Label1_Click_1);
+            // 
+            // lbl_current_file_name
+            // 
+            this.lbl_current_file_name.AutoSize = true;
+            this.lbl_current_file_name.Location = new System.Drawing.Point(121, 44);
+            this.lbl_current_file_name.Name = "lbl_current_file_name";
+            this.lbl_current_file_name.Size = new System.Drawing.Size(0, 17);
+            this.lbl_current_file_name.TabIndex = 4;
+            // 
+            // datagrid_ram
+            // 
+            this.datagrid_ram.AllowUserToResizeRows = false;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Garamond", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.datagrid_ram.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            this.datagrid_ram.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.datagrid_ram.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Lable,
+            this.Address,
+            this.Instruction,
+            this.Hex});
+            this.datagrid_ram.GridColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.datagrid_ram.Location = new System.Drawing.Point(710, 81);
+            this.datagrid_ram.Name = "datagrid_ram";
+            this.datagrid_ram.ReadOnly = true;
+            this.datagrid_ram.RowTemplate.Height = 24;
+            this.datagrid_ram.Size = new System.Drawing.Size(445, 622);
+            this.datagrid_ram.TabIndex = 6;
+            this.datagrid_ram.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DataGridView1_CellContentClick);
+            // 
+            // Lable
+            // 
+            this.Lable.HeaderText = "Lable";
+            this.Lable.Name = "Lable";
+            this.Lable.ReadOnly = true;
+            // 
+            // Address
+            // 
+            this.Address.HeaderText = "Address";
+            this.Address.Name = "Address";
+            this.Address.ReadOnly = true;
+            // 
+            // Instruction
+            // 
+            this.Instruction.HeaderText = "Instruction";
+            this.Instruction.Name = "Instruction";
+            this.Instruction.ReadOnly = true;
+            // 
+            // Hex
+            // 
+            this.Hex.HeaderText = "Hex";
+            this.Hex.Name = "Hex";
+            this.Hex.ReadOnly = true;
+            // 
+            // form1BindingSource
+            // 
+            this.form1BindingSource.DataSource = typeof(BasicCompiuter.Form1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1178, 749);
+            this.Controls.Add(this.datagrid_ram);
+            this.Controls.Add(this.lbl_current_file_name);
+            this.Controls.Add(this.lbl_file_name);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.lbl_code);
+            this.Controls.Add(this.txt_code);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Basic Computer";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.datagrid_ram)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.form1BindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txt_code;
+        private System.Windows.Forms.Label lbl_code;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripSplitButton toolStripSplitButton1;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openFileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog2;
+        private System.Windows.Forms.Label lbl_file_name;
+        private System.Windows.Forms.Label lbl_current_file_name;
+        private System.Windows.Forms.SaveFileDialog loadFileDialog;
+        private System.Windows.Forms.DataGridView datagrid_ram;
+        private System.Windows.Forms.BindingSource form1BindingSource;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Lable;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Address;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Instruction;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Hex;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +17,116 @@ namespace BasicCompiuter
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //set default numbers in Ram table
+           // datagrid_ram.Rows.Add(1024);
+
+            for(int i=0; i<1024; i++)
+            {
+                datagrid_ram.Rows.Add("" , Convert.ToString(i), "", "");
+            }
+
+
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Txt_code_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SaveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (txt_code.Text == "")
+            {
+                MessageBox.Show("Please Enter Your Code", "Error");
+
+            }
+            else
+            {
+                //saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+                //saveFileDialog2.ShowDialog();
+                saveFileDialog2.Title = "Save File";
+                saveFileDialog2.DefaultExt = "txt";
+                saveFileDialog2.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+                saveFileDialog2.FilterIndex = 2;
+                //saveFileDialog2.CheckFileExists = true;
+                saveFileDialog2.CheckPathExists = true;
+
+                
+
+
+                if (saveFileDialog2.ShowDialog() == DialogResult.OK)
+                {
+                 
+                    string save_file_name = saveFileDialog2.FileName;
+                    Console.WriteLine(save_file_name);
+                    // Write to the file name selected.
+                    // ... You can write the text from a TextBox instead of a string literal.
+                    File.WriteAllText(save_file_name, txt_code.Text );
+                    string[] split_name = save_file_name.Split('\\');
+                    lbl_current_file_name.Text = split_name[split_name.Length - 1];
+
+                    //txt_code.Text = saveFileDialog1.FileName;
+                }
+
+
+
+            }
+
+        }
+
+        private void Label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void OpenFileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            //saveFileDialog2.ShowDialog();
+            saveFileDialog2.Title = "Load File";
+            saveFileDialog2.DefaultExt = "txt";
+            saveFileDialog2.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            saveFileDialog2.FilterIndex = 2;
+            saveFileDialog2.CheckFileExists = true;
+            saveFileDialog2.CheckPathExists = true;
+
+
+
+
+            if (saveFileDialog2.ShowDialog() == DialogResult.OK)
+            {
+
+                string save_file_name = saveFileDialog2.FileName;
+                Console.WriteLine(save_file_name);
+                // Write to the file name selected.
+                // ... You can write the text from a TextBox instead of a string literal.
+                //File.WriteAllText(save_file_name, txt_code.Text);
+                txt_code.Text = File.ReadAllText(save_file_name);
+                string[] split_name = save_file_name.Split('\\');
+                lbl_current_file_name.Text = split_name[split_name.Length - 1];
+
+                //txt_code.Text = saveFileDialog1.FileName;
+            }
+        }
+
+        private void ExitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Goodbye");
+            Application.Exit();
+        }
+
+        private void DataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
         }
     }
 }
